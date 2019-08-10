d3759fb5b3df065a4a0319a1e467388ef75c5fcd
Vein <trojan_ann@aliyun.com>
2019-08-10 10:53:51 +0800

    修改打包位置

---
+++ introduce-of-es6/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/front-end-knowlege-slides/introduce-of-es6/'
+      : '/',
+  outputDir: '../docs/introduce-of-es6/',
+}
