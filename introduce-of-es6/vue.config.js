4e01507a0fd01d924f6a5520e18e7a0d4d03267d
Vein <trojan_ann@aliyun.com>
2020-02-02 21:14:05 +0800

    assets打包位置分开

--- introduce-of-es6/vue.config.js
+++ introduce-of-es6/vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? 'https://cdn.jsdelivr.net/npm/introduce-of-es6@0.1.2/docs/'
+      ? `https://cdn.jsdelivr.net/npm/introduce-of-es6@0.1.3/assets/`
       : '/',
   outputDir: '../docs/introduce-of-es6/',
+  assetsDir: '../../introduce-of-es6/assets/',
   devServer: {
     open: true,
   },
