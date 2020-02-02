c2edc78639f69209e25e7be7827dcf05c58336a5
Vein <trojan_ann@aliyun.com>
2020-02-02 20:28:29 +0800

    修改版本号0.1.2

--- introduce-of-es6/vue.config.js
+++ introduce-of-es6/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? 'https://cdn.jsdelivr.net/npm/introduce-of-es6@0.1.0/docs/'
+      ? 'https://cdn.jsdelivr.net/npm/introduce-of-es6@0.1.2/docs/'
       : '/',
   outputDir: '../docs/introduce-of-es6/',
   devServer: {
