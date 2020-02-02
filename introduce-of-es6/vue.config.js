077a6f5364f16800ca269014cb63ba525da33c0d
Vein <trojan_ann@aliyun.com>
2020-02-02 20:17:11 +0800

    使用jsDeliv的cdn

--- introduce-of-es6/vue.config.js
+++ introduce-of-es6/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? '/front-end-knowlege-slides/introduce-of-es6/'
+      ? 'https://cdn.jsdelivr.net/npm/introduce-of-es6@0.1.0/docs/'
       : '/',
   outputDir: '../docs/introduce-of-es6/',
   devServer: {
